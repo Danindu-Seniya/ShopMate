658074ed82bee9d0cf17a527f42009ddb406c8d0
Danindu-Seniya <danindunawa@gmail.com>
2024-02-12 13:13:24 +0530

    Updated AR Navigation Screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,21 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, TouchableOpacity } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { SimpleLineIcons } from '@expo/vector-icons';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>AR Navigation</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <SimpleLineIcons name="location-pin" size={150} color="#727272" />
+      <Text style={styles.title}>Navigate Easily</Text>
+      <Text style={styles.sentence}>Find your destination easy and fast with Augmented Reality</Text>
+
+      <View style={styles.buttonWrapper}>
+        <Text style={styles.buttonWrapperText}>Click Next to start your Navigation </Text>
+        <TouchableOpacity style={styles.button} onPress={() => console.log('hello')}>
+          <Text style={styles.buttonText}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -18,14 +25,44 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 10,
   },
   title: {
-    fontSize: 20,
+    fontSize: 35,
     fontWeight: 'bold',
+    paddingTop: 10,
+  },
+  sentence:{
+    fontSize: 14,
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 100,
+    borderRadius: 8,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  buttonWrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 150, 
+  },
+  buttonWrapperText: {
+    fontWeight: 'bold',
+    paddingVertical: 10, 
   },
 });
