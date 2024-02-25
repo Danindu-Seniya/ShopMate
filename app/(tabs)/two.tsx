f7c056a9fb49d421a683dbb0d4babf92b69829e1
Danindu-Seniya <danindunawa@gmail.com>
2024-02-26 00:20:40 +0530

    Modified tailwind config file.

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -5,7 +5,7 @@ import { Text, View } from '@/components/Themed';
 export default function TabTwoScreen() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text>Home</Text>
+      <Text className="text-black">Home</Text>
       </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  content: ["./_layout.{js,jsx,ts,tsx}", "./(tabs)/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
