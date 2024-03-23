df02590a6b7a6567af190a8e15a3bccba5ba223f
Danindu-Seniya <danindunawa@gmail.com>
2024-03-23 10:00:29 +0530

    Updated emergency services tab.

---
+++ app/(tabs)/emergency.tsx
@@ -0,0 +1,53 @@
+import { View, Text, ImageBackground, TouchableOpacity ,StyleSheet} from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const emergency = () => {
+  return (
+    <View>
+        <SafeAreaView>
+        <ImageBackground source={require('../../assets/images/EmergencyTabBG.png')} style={{ width: '100%', height: '100%' }} />
+
+        <TouchableOpacity
+    style={styles.editButton}
+    onPress={() => handleEditBio()}
+  >
+    <Text style={styles.editButtonText}>Edit Bio</Text>
+  </TouchableOpacity>
+
+  <TouchableOpacity
+    style={styles.editButton}
+    onPress={() => handleEditBio()}
+  >
+    <Text style={styles.editButtonText}>Edit Bio</Text>
+  </TouchableOpacity>
+
+  <TouchableOpacity
+    style={styles.editButton}
+    onPress={() => handleEditBio()}
+  >
+    <Text style={styles.editButtonText}>Edit Bio</Text>
+  </TouchableOpacity>
+
+      </SafeAreaView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    editButton: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        
+        borderRadius: 5,
+        padding: 10,
+    },
+    editButtonText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+
+})
+
+export default emergency
