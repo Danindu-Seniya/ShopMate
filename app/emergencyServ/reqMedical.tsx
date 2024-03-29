a823cb2d34e86fb728a29fc30fe0253108644487
shenaraDiv <shenara.20220858@iit.ac.lk>
2024-03-29 23:10:50 +0530

    reqEmergency styling done

--- app/emergencyServ/reqMedical.tsx
+++ app/emergencyServ/reqMedical.tsx
@@ -5,32 +5,31 @@ import { SimpleLineIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { Link } from "expo-router";
 import {MaterialIcons} from "@expo/vector-icons";
-import { Fontisto } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons'; 
 
 export default function reqMedical() {
   return (
     <View style={styles.container}>
-     <Fontisto name="map-marker-alt" size={150} color="red" />
+     <Fontisto name="map-marker-alt" size={150} color="red" bottom= {90} />
      <Text style={styles.title}>Share Your Location</Text>
      <Text style={styles.sentence}>
      Scan the closest shop front and{"\n"} 
      connect with the service.
      </Text>
 
-     <View style={styles.buttonWrapper}>
+     
 
        <Text style={styles.buttonWrapperText}>
          Click camera to connect{" "}
        </Text>
 
-       <View style={styles.buttonWrapper}>
+       
         <TouchableOpacity style={styles.button}>
           <MaterialIcons name="photo-camera" size={24} color="white" />
         </TouchableOpacity>
         </View>
        
-     </View>
-   </View>
+     
  );
 }
 
@@ -40,17 +39,19 @@ const styles = StyleSheet.create({
    alignItems: "center",
    justifyContent: "center",
    paddingTop: 10,
+   backgroundColor: '#EEF0FA'
  },
  title: {
    fontSize: 35,
    fontWeight: "bold",
    paddingTop: 10,
-   bottom: -100
+   bottom: 95
  },
  sentence: {
    fontSize: 14,
    alignItems: "center",
-
+  textAlign: "center",
+  bottom: 95,
  },
  
  button: {
@@ -61,7 +62,7 @@ const styles = StyleSheet.create({
   borderRadius: 30,
   elevation: 3,
   backgroundColor: "#2E77E5",
-  bottom: 100,
+  bottom: -60,
  },
  buttonText: {
    fontSize: 16,
