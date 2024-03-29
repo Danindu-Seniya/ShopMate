8f759bee5ff225f5f89d503e756d79fcd01cbeb6
shenaraDiv <shenara.20220858@iit.ac.lk>
2024-03-29 23:17:24 +0530

    reqsecurity styling done

--- app/emergencyServ/reqSecurity.tsx
+++ app/emergencyServ/reqSecurity.tsx
@@ -5,76 +5,81 @@ import { SimpleLineIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { Link } from "expo-router";
 import {MaterialIcons} from "@expo/vector-icons";
+import { Fontisto } from '@expo/vector-icons'; 
 
 export default function reqSecurity() {
   return (
     <View style={styles.container}>
-     <MaterialIcons name="emergency-share" size={160} color="#727272" />
+     <Fontisto name="map-marker-alt" size={150} color="red" bottom= {90} />
      <Text style={styles.title}>Share Your Location</Text>
      <Text style={styles.sentence}>
-       {"Scan the closest shop front and connect with the service."}
+       Scan the closest shop front and{"\n"}connect with the service.
      </Text>
 
-     <View style={styles.buttonWrapper}>
+     
 
        <Text style={styles.buttonWrapperText}>
          Click camera to locate{" "}
        </Text>
 
-       <View style={styles.buttonWrapper}>
+       
         <TouchableOpacity style={styles.button}>
           <MaterialIcons name="photo-camera" size={24} color="white" />
         </TouchableOpacity>
         </View>
        
-     </View>
-   </View>
+     
  );
 }
 
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 10,
+    backgroundColor: '#EEF0FA'
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "bold",
+    paddingTop: 10,
+    bottom: 95
+  },
+  sentence: {
+    fontSize: 14,
+    alignItems: "center",
+   textAlign: "center",
+   bottom: 95,
+  },
+  
+  button: {
    alignItems: "center",
    justifyContent: "center",
-   paddingTop: 10,
- },
- title: {
-   fontSize: 35,
-   fontWeight: "bold",
-   paddingTop: 10,
- },
- sentence: {
-   fontSize: 14,
- },
- separator: {
-   marginVertical: 30,
-   height: 1,
-   width: "80%",
- },
- button: {
-  alignItems: "center",
-  justifyContent: "center",
-  width: 60,
-  height: 60,
-  borderRadius: 30,
-  elevation: 3,
-  backgroundColor: "black",
-},
- buttonText: {
-   fontSize: 16,
-   lineHeight: 21,
-   fontWeight: "bold",
-   letterSpacing: 0.25,
-   color: "white",
- },
- buttonWrapper: {
-   alignItems: "center",
-   justifyContent: "center",
-   paddingTop: 150,
- },
- buttonWrapperText: {
-   fontWeight: "bold",
-   paddingVertical: 10,
- },
-});
+   width: 60,
+   height: 60,
+   borderRadius: 30,
+   elevation: 3,
+   backgroundColor: "#2E77E5",
+   bottom: -60,
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+    bottom: -100,
+  },
+  buttonWrapper: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 150,
+  },
+  buttonWrapperText: {
+    fontWeight: "bold",
+    paddingVertical: 10,
+    bottom: -60,
+  },
+ });
+ 
