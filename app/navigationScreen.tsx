42d299281459021eac279710a8d2c380ace51c43
shenaraDiv <shenara.20220858@iit.ac.lk>
2024-03-31 18:40:19 +0530

    Update navigation shops

--- app/navigationScreen.tsx
+++ app/navigationScreen.tsx
@@ -9,13 +9,13 @@ const SearchableShopList = () => {
   // Shop data
   const shops = [
     { id: 1, name: 'Keells', logo: require("../assets/images/Keells.jpg"), rating: '4.5', category: 'Food City' },
-    { id: 2, name: 'Shop 2', logo: require("../assets/images/ice-cream.png"), rating: '5.0', category: 'Electronics' },
-    { id: 3, name: 'Shop 1', logo: require("../assets/images/Kickspot.jpeg"), rating: '4.5', category: 'Clothing' },
-    { id: 4, name: 'Shop 2', logo: require("../assets/images/ice-cream.png"), rating: '5.0', category: 'Electronics' },
-    { id: 5, name: 'Shop 1', logo: require("../assets/images/Kickspot.jpeg"), rating: '4.5', category: 'Clothing' },
-    { id: 6, name: 'Shop 2', logo: require("../assets/images/ice-cream.png"), rating: '5.0', category: 'Electronics' },
-    { id: 7, name: 'Shop 1', logo: require("../assets/images/Kickspot.jpeg"), rating: '4.5', category: 'Clothing' },
-    { id: 8, name: 'Shop 2', logo: require("../assets/images/ice-cream.png"), rating: '5.0', category: 'Electronics' },
+    { id: 2, name: 'Odel', logo: require("../assets/images/odel.jpeg"), rating: '5.0', category: 'Clothing' },
+    { id: 3, name: 'Softlogic Max', logo: require("../assets/images/SM.png"), rating: '4.5', category: 'Electronics' },
+    { id: 4, name: 'Vision Care', logo: require("../assets/images/VC.jpeg"), rating: '5.0', category: 'Electronics' },
+    { id: 5, name: 'Roots', logo: require("../assets/images/Roots.jpg"), rating: '4.5', category: 'Food' },
+    { id: 6, name: 'Baskin Robbins', logo: require("../assets/images/BR.jpg"), rating: '5.0', category: 'Food' },
+    { id: 7, name: 'Burger King', logo: require("../assets/images/BK.png"), rating: '4.5', category: 'Food' },
+    { id: 8, name: 'Maxx lite', logo: require("../assets/images/ice-cream.png"), rating: '5.0', category: 'Cinema' },
   ];
 
   // Filter shops based on search text
